6e9706c0dde25a8513f13a1b6336bd1fb750edb3
Shakya Majumdar <shakyamajumdar1@gmail.com>
2021-11-09 10:49:45 +0530

    add implementation of Dijkstra

--- pathfinding/solvers/dijkstra_solver.py
+++ pathfinding/solvers/dijkstra_solver.py
@@ -1,12 +1,43 @@
+from collections import defaultdict
 from collections.abc import Iterable, Sequence
 
-from ..data_structures import Position, SolveStep
+from ..data_structures import Heap, Position, PriorityQueue, SolveStep
+from ..maze import MazeVertex
 from .maze_solver_abc import MazeSolver
 
 
 class DijkstraSolver(MazeSolver):
     def solve(self) -> Iterable[Position]:
-        pass
+        graph = self.maze.to_graph()
+        source = next(vertex for vertex in graph.vertices if vertex.data == self.maze.entry_point)
+        visited: set[MazeVertex] = {source}
+        self.shortest_distances: defaultdict[MazeVertex, float] = defaultdict(lambda: float("inf"))
+        self.shortest_distances[source] = 0
+        priority_queue: PriorityQueue[MazeVertex] = Heap()
+
+        for vertex in graph.vertices:
+            priority_queue.push(vertex, self.shortest_distances[vertex])
+
+        while priority_queue:
+            vertex = priority_queue.pop()
+            visited.add(vertex)
+            for edge in vertex.edges:
+
+                other_end = edge.get_other_end(vertex)
+
+                if other_end in visited:
+                    continue
+                new_dist = min(self.shortest_distances[other_end], self.shortest_distances[vertex] + len(edge.data))
+                print("upd", other_end.data, vertex.data, new_dist, self.shortest_distances[vertex])
+                self.shortest_distances[other_end] = new_dist
+                priority_queue.decrease_priority(other_end, new_dist)
+
+            yield vertex.data
+
+        self._completed = True
 
     def get_shortest_path(self) -> Sequence[SolveStep]:
-        pass
+        if not self._completed:
+            raise RuntimeError("solve method was not called")
+
+        return []
